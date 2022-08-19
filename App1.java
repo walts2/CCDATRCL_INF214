a55359c696758a7fb5ae5e512a70d68260e016e8
Elaine Victoria <111344174+wjsnleeluda@users.noreply.github.com>
2022-08-19 14:51:14 +0800

    Update App1.java

--- App1.java
+++ App1.java
@@ -1,14 +1,14 @@
 import java.util.*;
-public class App1 {
+public class App {
     
     public static void main(String[] args) throws Exception {
 
         Queue<String> restaurantreserve = new LinkedList<>();
-        restaurantreserve.add(e: "Walt"); 
-        restaurantreserve.add(e: "Kaede");  
-        restaurantreserve.add(e: "Kiel");  
-        restaurantreserve.add(e: "Aleck");   
-        restaurantreserve.add(e: "Clemard");  
+        restaurantreserve.add("Walt"); 
+        restaurantreserve.add("Kaede");  
+        restaurantreserve.add("Kiel");  
+        restaurantreserve.add("Aleck");   
+        restaurantreserve.add("Clemard");  
 
         System.out.println("The queue is: " + restaurantreserve);
         restaurantreserve.remove();
